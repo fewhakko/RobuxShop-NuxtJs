5d79bd8c95692d1ba104b61ed015570b7e8c8d88
Nong Chuy <thtongame@gmail.com>
2023-12-04 20:03:24 +0700

    Create app.vue
    test

---
+++ app.vue
@@ -0,0 +1,21 @@
+<script setup>
+useSeoMeta({
+  title: 'Meet Nuxt',
+  description: 'The Intuitive Vue Framework.'
+})
+</script>
+
+<template>
+  <div id="app">
+    <AppHeader />
+    <NuxtPage />
+    <AppFooter />
+  </div>
+</template>
+
+<style>
+#app {
+  background-color: #020420;
+  color: #00DC82;
+}
+</style>
